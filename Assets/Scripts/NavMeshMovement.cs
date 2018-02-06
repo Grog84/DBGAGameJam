ab2e64b404af013a5d3478e66b79115cc1ea0cc4
GiuliaMarchesini <giuliamarches@gmail.com>
2018-02-06 18:11:48 +0100

    Added NavPoints and switch of navigation targets

--- Assets/Scripts/NavMeshMovement.cs
+++ Assets/Scripts/NavMeshMovement.cs
@@ -6,19 +6,53 @@ using UnityEngine.AI;
 public class NavMeshMovement : MonoBehaviour
 {
 	public NavMeshAgent agent;
-	public GameObject target;
+	public Transform currentTarget;
+	private int currentTargetIndex = 0;
+	//private Transform lastTarget;
+	public List<Transform> targets;
+
+
+	private void Start()
+	{
+		currentTarget = targets[currentTargetIndex];
+		agent.destination = currentTarget.position;
+	}
 
 	private void Update()
 	{
-		MoveOnNavMesh();
+		MoveOnNavMesh(currentTarget);
+
+		if (currentTarget != null
+				&& (new Vector2(currentTarget.transform.position.x, currentTarget.transform.position.z) - new Vector2(agent.transform.position.x, agent.transform.position.z)).sqrMagnitude
+				<= agent.stoppingDistance)
+		{
+			OnTargetReached();
+		}
 	}
 
-	private void MoveOnNavMesh()
+	private void MoveOnNavMesh(Transform target)
 	{
 		if (target != null)
 		{
 			agent.isStopped = false;
-			agent.SetDestination(target.transform.position);
+			agent.destination = currentTarget.position;
 		}
+	}
+
+	private void OnTargetReached()
+	{
+		agent.isStopped = true;
+		//lastTarget = currentTarget;
+		if (agent.enabled && currentTargetIndex < targets.Count - 1)
+		{
+			ChangeNavPoint();
+		}
+		agent.isStopped = false;
+	}
+
+	void ChangeNavPoint()
+	{
+		currentTargetIndex++;
+		currentTarget = targets[currentTargetIndex];
 	}
 }
---
+++ Assets/Scripts/NavPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavPoint : MonoBehaviour
+{
+	private MeshRenderer meshRenderer;
+
+	private void Start()
+	{
+		meshRenderer = GetComponent<MeshRenderer>();
+		meshRenderer.enabled = false;
+	}
+
+}
