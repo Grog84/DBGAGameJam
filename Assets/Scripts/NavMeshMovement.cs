37ad6dbbd92aee5cf918d7292ddd58f63ce150cc
GiuliaMarchesini <giuliamarches@gmail.com>
2018-02-06 15:59:14 +0100

    Added NavMesh

---
+++ Assets/Scripts/NavMeshMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshMovement : MonoBehaviour
+{
+	public NavMeshAgent agent;
+	public GameObject target;
+
+	private void Update()
+	{
+		MoveOnNavMesh();
+	}
+
+	private void MoveOnNavMesh()
+	{
+		if (target != null)
+		{
+			agent.isStopped = false;
+			agent.SetDestination(target.transform.position);
+		}
+	}
+}
