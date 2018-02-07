cde8f7650078aa7159d8b148e021a1078c506433
FabioBrigidi <fabio.brigidi@gmail.com>
2018-02-07 20:34:20 +0100

    Created bomb prefab

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -6,7 +6,15 @@ public class Bomb : MonoBehaviour {
 
     [Range(1, 10)]
     public int numberOfRequiredTap = 3;
-    public int numberOfTap = 0;
+    [HideInInspector] public int numberOfTap = 0;
+
+    public GameObject explosion;
+    SpriteRenderer m_Renderer;
+
+    private void Awake()
+    {
+        m_Renderer = GetComponent<SpriteRenderer>();
+    }
 
     private void OnMouseDown()
     {
@@ -15,17 +23,23 @@ public class Bomb : MonoBehaviour {
 
     private void Explode()
     {
-
+        explosion.SetActive(true);
     }
 
     private void Update()
     {
         if (numberOfTap > numberOfRequiredTap)
         {
+            m_Renderer.enabled = false;
+            Explode();
 
+        }
+        if (numberOfTap == -1)
+        {
+            Destroy(gameObject);
         }
     }
 
-
+    
 
 }
