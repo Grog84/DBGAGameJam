d0ee7110316a267dd150f50ff96b78514809fd52
FabioBrigidi <fabio.brigidi@gmail.com>
2018-02-07 18:15:27 +0100

    Created pastorella prefab

---
+++ Assets/Scripts/Pastorella.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pastorella : MonoBehaviour {
+
+    HeroManager heroManager;
+
+    private void Start()
+    {
+        heroManager = FindObjectOfType<HeroManager>();
+    }
+
+    void CharmPlayer()
+    {
+        heroManager.startCharmed = true;
+    }
+
+    void EndCharmPlayer()
+    {
+        heroManager.endCharmed = true;
+    }
+
+    public void Disappear()
+    {
+        Destroy(gameObject);
+    }
+
+}
