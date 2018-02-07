c787f051ca7d8148ee55afc2b50a6070a329e946
FabioBrigidi <fabio.brigidi@gmail.com>
2018-02-07 19:39:51 +0100

    Modified pastorella navigation params

---
+++ Assets/Scripts/MulinoTeleport.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MulinoTeleport : MonoBehaviour {
+
+    public GameObject[] links;
+
+    private void Start()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            GameObject farestLink = links[0];
+            float dist = (farestLink.transform.position - transform.position).sqrMagnitude;
+            if ((links[1].transform.position - transform.position).sqrMagnitude > dist)
+            {
+                farestLink = links[1];
+            }
+            other.transform.position = farestLink.transform.position;
+
+        }
+    }
+}
