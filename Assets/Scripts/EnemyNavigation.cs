68c0f8d52d30100c95be2bd8641500e6fd992f84
FabioBrigidi <fabio.brigidi@gmail.com>
2018-02-07 12:46:13 +0100

    Started imlmenting the coschiotte animation

--- Assets/Scripts/EnemyNavigation.cs
+++ Assets/Scripts/EnemyNavigation.cs
@@ -7,8 +7,9 @@ public class EnemyNavigation : NavigationController
 {
 	public Transform hero;
 
-	private void Start()
+	protected override void Start()
 	{
+        base.Start();
 		currentTarget = hero;
 		agent.destination = currentTarget.position;
 	}
