8f44820cb092e94ed2cadf68c1c039cbd0575807
GiuliaMarchesini <giuliamarches@gmail.com>
2018-02-07 18:36:56 +0100

    Added all village people animations and blend trees

---
+++ Assets/Scripts/UIAnimationController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIAnimationController : MonoBehaviour {
+
+	Animator m_Animator;
+	public int animType;
+
+
+	// Use this for initialization
+	void Start () {
+		m_Animator = GetComponent<Animator>();
+		m_Animator.SetFloat("Selection", (float)animType);
+	}
+	
+	
+}
