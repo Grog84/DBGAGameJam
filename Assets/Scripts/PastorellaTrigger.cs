58e4b4159f03c9125b8f5e8fc4c73a9848a46afb
FabioBrigidi <fabio.brigidi@gmail.com>
2018-02-07 18:25:27 +0100

    Created pastorella activation trigger

---
+++ Assets/Scripts/PastorellaTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PastorellaTrigger : MonoBehaviour {
+
+    public GameObject pastorella;
+
+    private void Awake()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Hero")
+        {
+            pastorella.SetActive(true);
+        }
+    }
+}
