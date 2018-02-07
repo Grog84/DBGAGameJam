45bb39ac9967678dc82e2e6205960ce5c0cd34e1
GiuliaMarchesini <giuliamarches@gmail.com>
2018-02-07 10:18:07 +0100

    Added GameManager, pause function (temporarily mapped on esc), and fade to/from black functions

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour
+{
+	private bool isPaused = false;
+
+	public Image blackImage; // Black image used for fade effect
+	public float fadeDuration;
+
+	private void Start()
+	{
+		FadeFromBlack();
+	}
+
+	private void Update()
+	{
+		// Input provvisorio
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			PauseGame();
+		}
+	}
+
+	public void PauseGame()
+	{
+		if (isPaused)
+		{
+			Time.timeScale = 1;
+			isPaused = false;
+		}
+		else
+		{
+			Time.timeScale = 0;
+			isPaused = true;
+		}
+	}
+
+	public void FadeFromBlack()
+	{
+		blackImage.DOFade(0, fadeDuration);
+	}
+
+	public void FadeToBlack()
+	{
+		blackImage.DOFade(1, fadeDuration);
+	}
+}
