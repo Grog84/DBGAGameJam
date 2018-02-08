d2d4fa58a708266531d1a11724b7871ed8e9c2e6
saem <chionettisamuele@gmail.com>
2018-02-08 02:31:32 +0100

    un botto di cose belle (dio la madonna incarnita)

--- Assets/Scripts/Tronco.cs
+++ Assets/Scripts/Tronco.cs
@@ -43,7 +43,7 @@ public class Tronco : MonoBehaviour {
         {
             collision.gameObject.GetComponent<MouseDrag>().anim.SetTrigger("Explosion");
         }
-        else if (collision.gameObject.tag == "Wall")
+		else if (collision.gameObject.tag == "EndPoint")
         {
             DestroyTronco();
         }
